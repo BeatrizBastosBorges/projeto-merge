50ea3e22a3c41f94c14b86da9dbd054080a75f42
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-05-09 00:13:06 -0300

    Add files via upload

---
+++ pilhaArray.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <locale.h>
+
+/*Elabore um programa que permita digitar 30 números inteiros.
+
+Garantir que cada número digitado seja maior do que o anterior, mantendo a ordem crescente da digitação.
+
+Após digitado, cada número deverá ser empilhado em uma estrutura respectiva, de acordo com o fato dele ser par ou ímpar.
+
+Finalizada a digitação dos 30 números, os valores deverão ser desempilhados e mostrados na tela, garantindo uma sequência decrescente.
+
+Obs: Desenvolver essa solução implementando as pilhas em forma de lista encadeada e de vetor.*/
+
+using namespace std;
+#define MAX 30
+
+struct Pilha {
+    int qtde;
+    float elementos[MAX];
+};
+
+Pilha* init()
+{
+    Pilha* p = new Pilha;
+    p->qtde = 0;
+    return p;
+}
+
+int isEmpty(Pilha* p)
+{
+    return (p->qtde == 0);
+}
+
+int count(Pilha* p)
+{
+    return (p->qtde);
+}
+
+void freePilha(Pilha* p)
+{
+    free(p);
+}
+
+int push(Pilha* p, float v)
+{
+    int podeEmpilhar = (p->qtde < MAX);
+    if (podeEmpilhar)
+        p->elementos[p->qtde++] = v;
+
+    return podeEmpilhar;
+}
+
+int pop(Pilha* p)
+{
+    int podeDesempilhar = (p->qtde > 0);
+    float v;
+
+    if (podeDesempilhar)
+    {
+        v = p->elementos[p->qtde - 1];
+        p->qtde--;
+    }
+    else
+    {
+        v = - 1;
+    }
+        
+
+    return v;
+}
+
+void print(Pilha* p) {
+    cout << "\nQtde de elementos: " << count(p) << endl;
+    for (int i = p->qtde - 1; i >= 0; --i) {
+        cout << p->elementos[i] << endl;
+    }
+    cout << "--------------------" << endl;
+
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "");
+
+    Pilha* pilhaFloat;
+    pilhaFloat = new Pilha();
+    pilhaFloat = init();
+
+    Pilha* pilhaPar = new Pilha();
+    pilhaPar = init();
+
+    Pilha* pilhaImpar = new Pilha();
+    pilhaImpar = init();
+
+    cout << "Pilha vazia: " << (isEmpty(pilhaFloat) ? "Sim" : "Não") << endl;
+
+    cout << "\n";
+
+    cout << "Digite 30 valores para popular a lista. Cada valor deve ser maior que o anterior." << endl;
+
+    int valor = 0;
+    int aux = valor;
+    for (int i = 0; i < 30; i++)
+    {
+        cin >> valor;
+        while (valor < aux)
+        {   
+            cout << "O valor digitado é menor que o anterior. Digite novamente" << endl;
+            cin >> valor;
+        }
+
+        push(pilhaFloat, valor);
+        
+        aux = valor;
+        valor = 0;
+    }
+    
+    int aux2;
+    for(int j = 0; j < 30; j++)
+    {
+        aux2 = pilhaFloat->elementos[j];
+        if (aux2 % 2 == 0)
+            push(pilhaPar, aux2);
+
+        else
+            push(pilhaImpar, aux2);
+    }
+
+    cout << "Pilha completa: " << endl;
+    print(pilhaFloat);
+
+    cout << "\n";
+    cout << "\n";
+    
+    cout << "Pilha par: " << endl;
+    print(pilhaPar);
+    
+    cout << "\n";
+    cout << "\n";
+   
+    cout << "Pilha ímpar: " << endl;
+    print(pilhaImpar);
+    return 0;
+}
+
---
+++ pilhaPonteiro.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+#define MAX 30
+
+struct No
+{   
+    float dado;
+    struct No *prox;
+};
+
+struct Pilha
+{
+    No *topo;
+};
+
+Pilha* init()
+{
+    Pilha* p = new Pilha;
+    p->topo = NULL;
+    return p;
+}
+
+int isEmpty(Pilha* p)
+{
+    return (p->topo == NULL);
+}
+
+int count(Pilha *p)
+{
+    int i = 0;
+    No *no;
+    no = p->topo;
+    while (no != NULL)
+    {
+        i++;
+        no = no->prox;
+    }
+    return i;
+}
+
+void print(Pilha* p)
+{
+    No* no;
+    no = p->topo;
+    cout << "Qtde de elementos: " << count(p) << endl;
+    while (no != NULL) 
+    {
+        cout << no->dado << endl;
+        no = no->prox;
+    }
+    cout << "--------------------" << endl;
+}
+
+void freePilha(Pilha* p) 
+{
+    No* no;
+    no = p->topo;
+    while (no != NULL) 
+    {
+        No* aux = no->prox;
+        free(no);
+        no = aux;
+    }
+    free(p);
+}
+
+void push(Pilha* p, float v)
+{
+    No *no = new No;
+    no->dado = v;
+    no->prox = p->topo;
+    p->topo = no;
+}
+
+float pop(Pilha* p)
+{
+    float v;
+    int podeDesempilhar = (p->topo != NULL);
+    if (podeDesempilhar)
+    {
+        No *no = p->topo;
+        v = no->dado; 
+        p->topo = no->prox;
+        free(no);
+    }
+    else
+    {
+        v = -1;
+    }
+
+    return v;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "");
+
+    Pilha *pilhaFloat = new Pilha();
+    pilhaFloat = init();
+
+    Pilha *pilhaPar = new Pilha();
+    pilhaPar = init();
+
+    Pilha *pilhaImpar = new Pilha();
+    pilhaImpar = init();
+
+    cout << "Pilha vazia: " << (isEmpty(pilhaFloat) ? "SIM" : "NÃO") << endl;
+    
+    cout << "\n";
+
+    cout << "Digite 30 valores para popular a lista. Cada valor deve ser maior que o anterior." << endl;
+    int valor = 0;
+    int aux = valor;
+    for (int i = 0; i < 30; i++)
+    {
+        cin >> valor;
+        while (valor < aux)
+        {
+            cout << "O valor digitado é menor que o anterior. Digite novamente" << endl;
+            cin >> valor;
+        }
+
+        push(pilhaFloat, valor);
+
+        aux = valor;
+        valor = 0;
+    }
+    
+    cout << "\n";
+
+    cout << "Pilha completa: " << endl;
+    print(pilhaFloat);
+
+    for (int j = 0; j < 30; j++)
+    {
+        float aux2 = pop(pilhaFloat);
+        if ((int)aux2 % 2 == 0)
+            push(pilhaPar, aux2);
+        else
+            push(pilhaImpar, aux2);
+    }
+
+    cout << "\n";
+
+    cout << "Pilha par: " << endl;
+    print(pilhaPar);
+
+    cout << "\n";
+
+    cout << "Pilha ímpar: " << endl;
+    print(pilhaImpar);
+    
+    return 0;
+}
+
